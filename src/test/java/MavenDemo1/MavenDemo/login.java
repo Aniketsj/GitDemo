6d40423e5411369229fee28127987b88fa873f9e
AniketS <aniketjadhav2007@gmail.com>
2019-03-05 22:15:32 +0530

    this is first commit

---
+++ src/test/java/MavenDemo1/MavenDemo/login.java
@@ -0,0 +1,23 @@
+package MavenDemo1.MavenDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+	
+	WebDriver dr=new ChromeDriver();
+	
+	@Test
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "F:\\Selenium course\\Browser driver\\chromedriver.exe");
+		dr.manage().window().maximize();
+		dr.get("https://www.google.com");
+		dr.navigate().to("https://www.facebook.com/");
+		dr.navigate().back();
+		
+		dr.navigate().forward();
+	}
+
+}
